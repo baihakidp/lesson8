e25fc3b847b97517dd976aea44d2a17af382776b
Baihaki <baihakidemiandraputra@gmail.com>
2022-11-10 22:05:38 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+// function perkalian() {}
+// function pertambahan() {}
+// function pengurangan() {}
+// function pembagian() {}
+// function kuadrat() {}
+// function akar() {}
+// function calistung() {}
+// function ngoding() {}
+// function apawehayu() {}
+// function lah(kogitu) {
+//   return `hah ${kogitu}`;
+// }
+
+// const gwBingung = lah("kok bisa");
+// const makinBingung = lah("hmmm");
+// console.log(gwBingung);
+// console.log(makinBingung);
+
+// function greeting(param1, param2) {
+//   return `hello ${param1} hai ${param2}`;
+// }
+
+// const manggil = greeting("sayu", "andre");
+// const kepanggil = greeting("rudi", "gunawan");
+// console.log(manggil);
+// console.log(kepanggil);
+
+// 3 bulan, 10%, 40%, 70% baju 250000
+// function hitungDiskon(param) {
+//   const hargaBaju = 250000;
+//   const hitung = (hargaBaju * param) / 100;
+//   const setelahDiskon = hargaBaju - hitung;
+//   return setelahDiskon;
+// }
+
+// const januari = hitungDiskon(10);
+// const februari = hitungDiskon(40);
+// const maret = hitungDiskon(70);
+// console.log(januari);
+// console.log(februari);
+// console.log(maret);
+
+// function menghitungUsia(usia) {
+//   let time = new Date();
+//   const tahun = time.getFullYear();
+//   const hitungUsia = tahun - usia;
+//   return hitungUsia;
+// }
+// const baihaki = menghitungUsia(2003);
+// console.log(baihaki);
+
+const perpangkatan = (a, b) => {
+  const rumus = Math.pow(a, b);
+  return rumus;
+};
+const rumus = perpangkatan(2, 2);
+console.log(rumus);
